3006a137f4fbc63217959e9c2734f4016ae3c882
lucid7 <lucid7@lucid7.local>
2021-01-12 18:40:18 +0900

    add recvuntil

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "Pwntools Cpp",
+        "title": "Pwntoolscpp 1",
         "excerpt":"pwntools는 CTF에 관심있는 사람이라면 한번쯤은 들어봤을 법한 pwnable을 위한 전용 도구이다. (https://github.com/Gallopsled/pwntools) python으로 제작된 이 도구는 ctf에서 pwnable을 빠르게 진행할 수 있도록 각종 편의 기능을 보유하고 있어 매우 편리하다. 만약 비교적 최근에 pwnable을 시작한 newbie라면 hexray(IDA)와 pwntools 없이는 pwnable이 불가능할 수도 있을만큼 필수적인 도구로 자리매김하고 있다. 취미 생활로 CTF 풀이를...","categories": [],
         "tags": [],
-        "url": "https://lucid78.github.io/pwntools-cpp/",
+        "url": "https://lucid78.github.io/pwntoolscpp-1/",
+        "teaser": null
+      },{
+        "title": "Pwntoolscpp 2",
+        "excerpt":"recvuntil recvuntil() 함수는 이 함수에 전달된 파라미터 문자가 대상의 출력에서 발견될 때까지 읽어들이는 함수이다. C에서라면 read() 함수로 문자를 1바이트씩 읽으면서 delim 문자인지 확인을 하는 꽤 귀찮은 작업을 거쳐야 하지만, boost에서는 boost::asio::read_until이라는 함수가 이 기능을 지원한다. (https://www.boost.org/doc/libs/1_70_0/doc/html/boost_asio/reference/read_until.html) boost::asio::read_until()의 사용법은 아래와 같다. boost::asio::streambuf buf; // pipe에서 읽은 data를 저장하는 buffer buf.prepare(4096); //...","categories": [],
+        "tags": [],
+        "url": "https://lucid78.github.io/pwntoolscpp-2/",
         "teaser": null
       }]
